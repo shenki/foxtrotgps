0eb526d3b0232571549fa63a24bb42d2d6dec32a
Joshua Judson Rosen <rozzin@geekspace.com>
2010-05-23 20:09:00 -0400

    Recreated `Friend Info' dialogue in GladeXML, replacing create_window8().

--- src/interface.h
+++ src/interface.h
@@ -1,5 +1,4 @@
 GtkWidget* create_window3 (void);
-GtkWidget* create_window8 (void);
 GtkWidget* create_win13_biggeo (void);
 GtkWidget* create_dialog_geocode (void);
 GtkWidget* create_dialog_image_data (void);
